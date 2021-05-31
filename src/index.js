a0d51b6306f53d401715b23d6d07d89f879332c4
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2021-05-31 10:58:24 +0200

    adaptive cards hello world

--- src/index.js
+++ src/index.js
@@ -1,25 +1,84 @@
-import '@axa-ch/patterns-library-polyfill';
-import '@axa-ch/button';
-import './index.scss';
+import "@axa-ch/patterns-library-polyfill";
+import "@axa-ch/button";
+import "./index.scss";
+
+import * as AdaptiveCards from "adaptivecards";
 
 export default class PodAdaptiveCardsTestings {
   // elem is the DOM node where the pod will be atatched on
   // options contains the store and the rest is the data attributes of the pod
-  constructor(elem, { store, ...rest }) {
+  constructor(elem, { ...rest }) {
     this.elem = elem;
     this.options = rest;
 
-    // { get: ƒ, publish: ƒ, subscribe: ƒ, clean: ƒ, getAsync: f }
-    this.store = store;
-
-    this.store.subscribe('test', 'any-namespace', (value) => {
-      // eslint-disable-next-line no-console
-      console.info(`-> external mock pod passed ${value} to us!`);
-    });
     this.init();
   }
 
   init() {
-    this.elem.innerHTML = '<axa-button>Hello, im a Button</axa-button>';
+    this.elem.innerHTML = "<h1>Adaptive Cards Playground</h1>";
+
+    // Author a card
+    // In practice you'll probably get this from a service
+    // see http://adaptivecards.io/samples/ for inspiration
+    var card = {
+      type: "AdaptiveCard",
+      version: "1.0",
+      body: [
+        {
+          type: "Image",
+          url: "http://adaptivecards.io/content/adaptive-card-50.png",
+        },
+        {
+          type: "TextBlock",
+          text: "Hello **Adaptive Cards!**",
+        },
+      ],
+      actions: [
+        {
+          type: "Action.OpenUrl",
+          title: "Learn more",
+          url: "http://adaptivecards.io",
+        },
+        {
+          type: "Action.OpenUrl",
+          title: "GitHub",
+          url: "http://github.com/Microsoft/AdaptiveCards",
+        },
+      ],
+    };
+
+    // Create an AdaptiveCard instance
+    var adaptiveCard = new AdaptiveCards.AdaptiveCard();
+
+    // Set its hostConfig property unless you want to use the default Host Config
+    // Host Config defines the style and behavior of a card
+    adaptiveCard.hostConfig = new AdaptiveCards.HostConfig({
+      fontFamily: "Segoe UI, Helvetica Neue, sans-serif",
+      // More host config options
+    });
+
+    // Set the adaptive card's event handlers. onExecuteAction is invoked
+    // whenever an action is clicked in the card
+    adaptiveCard.onExecuteAction = function (action) {
+      alert("Ow!");
+    };
+
+    // For markdown support you need a third-party library
+    // E.g., to use markdown-it, include in your HTML page:
+    //     <script type="text/javascript" src="https://unpkg.com/markdown-it/dist/markdown-it.js"></script>
+    // And add this code to replace the default markdown handler:
+    //     AdaptiveCards.AdaptiveCard.onProcessMarkdown = function (text, result) {
+    //         result.outputHtml = markdownit().render(text);
+    //         result.didProcess = true;
+    //     };
+
+    // Parse the card payload
+    adaptiveCard.parse(card);
+
+    // Render the card to an HTML element:
+    var renderedCard = adaptiveCard.render();
+
+    // And finally insert it somewhere in your page:
+    document.body.appendChild(renderedCard);
   }
 }
