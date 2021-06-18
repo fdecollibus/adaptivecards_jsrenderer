814a01752771bb6ee054bf6f1c4bbcd0769bbd37
Francesco deCollibus <francesco.decollibus@business.uzh.ch>
2021-06-18 22:07:15 +0200

    Added validator for datepicker

--- src/pl-datepicker.ts
+++ src/pl-datepicker.ts
@@ -15,10 +15,58 @@ export class PLDateInput extends AC.DateInput {
     datepicker.setAttribute('inputfield', "true");
     datepicker.setAttribute('errorMessage', this.errorMessage || '');
     datepicker.setAttribute('name', this.id || '');
+    datepicker.addEventListener("change", (e) => {
+      this.setValue(PLDateInput.valueProperty, (e.target as any).value);
+    });
     element.appendChild(datepicker);
     return element;
   }
+
+  // getJsonTypeName(): string {
+  //   return PLDateInput.JsonTypeName;
+  // }
+
+  // protected showValidationErrorMessage() {
+  //   debugger; 
+  // }
+  // public internalValidateProperties(context: ValidationResults) {
+  //  debugger;
+  // super.internalValidateProperties(context);
+  // };
+
+  static readonly valueProperty = new AC.StringProperty(
+    AC.Versions.v1_3,
+    "value"
+  );
+  @AC.property(PLDateInput.valueProperty)
+  public get value(): string | undefined {
+    return this.getValue(PLDateInput.valueProperty);
+  }
+  public set value(value: string | undefined) {
+    if (this.value !== value) {
+      this.setValue(PLDateInput.valueProperty, value);
+      this.updateLayout();
+    }
+  }
+  public isSet() {
+    return true;
+  }
+
+  public isValid() {
+    return true;
+  }
+
 }
+
+// defaultValue?: string;
+// min?: string;
+// max?: string;
+// placeholder?: string;
+// private _dateInputElement;
+// protected internalRender(): HTMLElement | undefined;
+// getJsonTypeName(): string;
+// isSet(): boolean;
+// isValid(): boolean;
 
 /*"type": "Input.Date",
 "label": "Wann ist der Schaden entstanden?",
